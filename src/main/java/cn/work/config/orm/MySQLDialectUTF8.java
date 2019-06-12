28ff3af9db84cf5c98e160ff1f5542c5d0b6358d
SiameseMiao <763692571@qq.com>
2019-06-12 18:51:52 +0800

    自动建表编码为UTF-8

---
+++ src/main/java/cn/work/config/orm/MySQLDialectUTF8.java
@@ -0,0 +1,16 @@
+package cn.work.config.orm;
+
+import org.hibernate.dialect.MySQL5InnoDBDialect;
+
+/**
+ * CompetitionDao Class
+ *
+ * @author <b>Siamese_miao</b>, Copyright &#169; 2018
+ * @version 1.0, 2019-06-12 18:40
+ */
+public class MySQLDialectUTF8 extends MySQL5InnoDBDialect {
+    @Override
+    public String getTableTypeString() {
+        return " ENGINE=InnoDB DEFAULT CHARSET=utf8";
+    }
+}
